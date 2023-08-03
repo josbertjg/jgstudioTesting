e1b9282d3901829f21d0e17a058378dcc9e40fd0
JOSBERT GUEDEZ <josbertjg@gmail.com>
2023-08-02 23:07:46 -0400

    Add basic template

--- views/admin_layout.php
+++ views/admin_layout.php
@@ -1,31 +1,101 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DevWebCamp - <?php echo $titulo; ?></title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/build/css/app.css">
-</head>
-<body class="dashboard">
-        <?php 
-            include_once __DIR__ .'/templates/admin-header.php';
-        ?>
-        <div class="dashboard__grid">
-            <?php
-                include_once __DIR__ .'/templates/admin-sidebar.php';  
-            ?>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <main class="dashboard__contenido">
-                <?php 
-                    echo $contenido; 
-                ?> 
-            </main>
+  <title>Gentelella Alela! | </title>
+
+  <!-- Bootstrap -->
+  <link href="/build/external_imports/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link href="/build/external_imports/css/font-awesome.min.css" rel="stylesheet">
+  <!-- NProgress -->
+  <link href="/build/external_imports/css/nprogress.css" rel="stylesheet">
+
+  <!-- Custom Theme Style -->
+  <link rel="stylesheet" href="/build/css/custom-gentella.css">
+</head>
+<body class="nav-md">
+  <?php 
+      include_once __DIR__ .'/templates/admin-header.php';
+  ?>
+  <!-- page content -->
+  <div class="right_col" role="main">
+    <div class="">
+      <div class="page-title">
+        <div class="title_left">
+          <h3>Plain Page</h3>
         </div>
 
-    <script src="/build/js/bundle.min.js" defer></script>
+        <div class="title_right">
+          <div class="col-md-5 col-sm-5 form-group pull-right top_search">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button">Go!</button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="clearfix"></div>
+
+      <div class="row">
+        <div class="col-md-12 col-sm-12  ">
+          <div class="x_panel">
+            <div class="x_title">
+              <h2>Plain Page</h2>
+              <ul class="nav navbar-right panel_toolbox">
+                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">Settings 1</a>
+                      <a class="dropdown-item" href="#">Settings 2</a>
+                    </div>
+                </li>
+                <li><a class="close-link"><i class="fa fa-close"></i></a>
+                </li>
+              </ul>
+              <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+              <?php 
+                echo $contenido; 
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /page content -->
+  <!-- footer content -->
+  <footer>
+    <div class="pull-right">
+      Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+  </footer>
+  <!-- /footer content -->
+  </div>
+</div>  
+    <!-- jQuery -->
+    <script src="/build/external_imports/js/jquery.min.js"></script>
+    <!-- Bootstrap -->
+   <script src="/build/external_imports/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="/build/external_imports/js/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="/build/external_imports/js/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="/build/js/custom.min.js"></script>
 </body>
 </html>
