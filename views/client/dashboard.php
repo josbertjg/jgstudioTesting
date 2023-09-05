1da299b500ac76038371270b9404e62b506ce112
JOSBERT GUEDEZ <josbertjg@gmail.com>
2023-09-05 16:20:16 -0400

    Add some refactors

--- views/client/dashboard.php
+++ views/client/dashboard.php
@@ -1,17 +1,17 @@
 <div class="jg-accordion-service-container flex-column flex-md-row align-items-md-start align-items-center">
   <?php 
-    foreach($servicios as $servicio){
-      if(count($servicio->productos)>0){
+    for($i = 0; $i<count($servicios);$i++){
+      if(count($servicios[$i]->productos)>0){
   ?>
-    <div class="accordion jg-accordion-service me-md-3 me-none my-3 my-md-none" id="servicio<?php echo $servicio->categoria->id ?>">
+    <div class="accordion jg-accordion-service me-md-3 me-none my-3 my-md-none" id="servicio<?php echo $servicios[$i]->categoria->id ?>">
       <div class="accordion-item">
         <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $servicio->categoria->id ?>" aria-expanded="true" aria-controls="collapse<?php echo $servicio->categoria->id ?>">
-            <span class="jg-service-title"><?php echo $servicio->categoria->nombre ?></span>
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $servicios[$i]->categoria->id ?>" aria-expanded="true" aria-controls="collapse<?php echo $servicios[$i]->categoria->id ?>">
+            <span class="jg-service-title"><?php echo $servicios[$i]->categoria->nombre ?></span>
             <img src="/build/img/rrss.png" alt="">
           </button>
         </h2>
-        <div id="collapse<?php echo $servicio->categoria->id ?>" class="accordion-collapse collapse" data-bs-parent="#servicio<?php echo $servicio->categoria->id ?>">
+        <div id="collapse<?php echo $servicios[$i]->categoria->id ?>" class="accordion-collapse collapse" data-bs-parent="#servicio<?php echo $servicios[$i]->categoria->id ?>">
           <div class="accordion-body">
             <form method="POST">
               <?php foreach($servicios[$i]->productos as $producto){ ?>
@@ -27,13 +27,12 @@
                   name="producto-<?php echo $producto->id ?>" 
                   value='<?php 
                     $objeto = (object)[];
-                    $objeto->id_categoria = $servicio->categoria->id;
-                    $objeto->nombre_categoria = $servicio->categoria->nombre;
+                    $objeto->id_categoria = $servicios[$i]->categoria->id;
+                    $objeto->nombre_categoria = $servicios[$i]->categoria->nombre;
                     $objeto->id_producto = $producto->id;
                     $objeto->nombre_producto = $producto->nombre;
                     $objeto->cantidad_producto = 0;
                     $objeto->item_id = uniqid();
-                    // print_r($objeto);
                     echo json_encode($objeto)
                   ?>'
                 >
@@ -53,14 +52,14 @@
                 <?php }else{ ?>
                 <div class="form-check form-switch product-input">
                   <input 
-                    class="form-check-input producto producto-categoria-<?php echo $servicio->categoria->id ?>" 
+                    class="form-check-input producto producto-categoria-<?php echo $servicios[$i]->categoria->id ?>" 
                     type="checkbox" 
                     role="switch" 
                     id="checkbox-producto-<?php echo $producto->id ?>" 
                     cantidad_maxima="<?php echo $producto->cantidad_maxima ?>" 
                     precio="<?php echo $producto->precio_unitario ?>" 
                     producto_id="<?php echo $producto->id ?>"
-                    categoria_id="<?php echo $servicio->categoria->id ?>"
+                    categoria_id="<?php echo $servicios[$i]->categoria->id ?>"
                   >
                   <label class="form-check-label product-input-title" for="checkbox-producto-<?php echo $producto->id ?>">Añadir <?php echo $producto->nombre ?>?</label>
                 </div>
@@ -69,9 +68,9 @@
                 <hr class="mb-1">
                 <div class="jg-service-total">
                   <span>Total: </span>
-                  <span> $ <span class="total total-<?php echo $servicio->categoria->id ?>">0.00</span></span>
+                  <span> $ <span class="total total-<?php echo $servicios[$i]->categoria->id ?>">0.00</span></span>
                 </div>
-              <input type="submit" class="btn btn-warning btn-block add-carrito-<?php echo $servicio->categoria->id ?>" value="Añadir al Carrito" disabled="true">
+              <input type="submit" class="btn btn-warning btn-block add-carrito-<?php echo $servicios[$i]->categoria->id ?>" value="Añadir al Carrito" disabled="true">
             </form>
           </div>
         </div>
@@ -81,37 +80,4 @@
       }
     }
   ?>
-
-
-
-  <!-- <div class="accordion jg-accordion-service me-md-3 me-none my-3 my-md-none" id="servicio2">
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <span class="jg-service-title">Titulo del Servicio</span>
-          <img src="/build/img/diseño.png" alt="">
-        </button>
-      </h2>
-      <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#servicio2">
-        <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="accordion jg-accordion-service me-md-3 me-none my-3 my-md-none" id="servicio3">
-    <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-          <span class="jg-service-title">Titulo del Servicio</span>
-          <img src="/build/img/diseño-web.png" alt="">
-        </button>
-      </h2>
-      <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#servicio3">
-        <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-  </div> -->
 </div>
