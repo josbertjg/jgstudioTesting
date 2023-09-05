50a088270f0b63ab6b27fea5cc23169598c29e5f
JOSBERT GUEDEZ <josbertjg@gmail.com>
2023-09-05 16:06:49 -0400

    Refactor estilos login y signin

--- views/auth/signin.php
+++ views/auth/signin.php
@@ -1,34 +1,39 @@
 <header id="inicio">
-    <h1><?php echo $titulo ?></h1>
-    <p><?php echo $descripcion ?></p>
-    <div>
-        <?php 
-            if($hasLogin){
-        ?>
-            <a href="/login">Login</a>
-        <?php
-            } if($hasSignin){
-        ?>
-            <a href="/signin">Registrate</a>
-        <?php 
-            } if($hasContact){
-        ?>
-            <a href="/#contacto">Contáctanos</a>
-        <?php } ?>
+    <div class="signin">
+        <h1><?php echo $titulo ?></h1>
+        <p><?php echo $descripcion ?></p>
+        <div>
+            <?php 
+                if($hasLogin){
+            ?>
+                <a href="/login">Login</a>
+            <?php
+                } if($hasSignin){
+            ?>
+                <a href="/signin">Registrate</a>
+            <?php 
+                } if($hasContact){
+            ?>
+                <a href="/#contacto">Contáctanos</a>
+            <?php } ?>
+        </div>
     </div>
 </header>
-<div class="container my-5">
+<div class="">
   <input type="hidden" class="jg-alert" value='<?php echo json_encode($alertas) ?>'>
-  <form method="POST" class="form">
-    <label for="" >Nombre</label>
-    <input type="text" name="nombre" class="form-control">
-    <label for="" >Apellido</label>
-    <input type="text" name="apellido" class="form-control">
-    <label for="" >Correo</label>
-    <input type="email" name="correo" class="form-control">
-    <label for="">Password</label>
-    <input type="password" name="clave" class="form-control">
-    <input type="submit" class="btn btn-primary">
-  </form>
-  <a href="/login">¿Ya tienes una cuenta?</a>
+  <div class="signin-form-container">
+    <div>
+        <h5>¡Que estas esperando!</h5>
+        <h1>Registrate</h1>
+        <p>Comienza a disfrutar de nuestros servicios <b>¡YA!</b></p>
+        <form method="POST" class="form">
+            <input type="text" name="nombre" class="form-control mb-3" placeholder="Nombre:">
+            <input type="text" name="apellido" class="form-control mb-3" placeholder="Apellido:">
+            <input type="email" name="correo" class="form-control mb-3" placeholder="Correo:">
+            <input type="password" name="clave" class="form-control mb-3" placeholder="Contraseña:">
+            <input type="submit" class="btn-submit">
+        </form>
+        <a href="/login">¿Ya tienes una cuenta?</a>
+    </div>
+    <img src="build/img/signin-form.png" alt="">
 </div>
